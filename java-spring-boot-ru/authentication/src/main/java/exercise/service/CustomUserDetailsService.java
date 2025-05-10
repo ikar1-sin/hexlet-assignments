61de6348c963afb2515e4f2a6fa25de224ee6cee
@hexlet/cli <support@hexlet.io>
2025-05-10 22:36:07 +0300

    submit java-spring-boot-ru/authentication

---
+++ java-spring-boot-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
@@ -0,0 +1,50 @@
+package exercise.service;
+
+import exercise.exception.ResourceNotFoundException;
+import exercise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+
+// BEGIN
+@Service
+public class CustomUserDetailsService implements  UserDetailsManager {
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User with email " + email + " nout found"));
+    }
+
+    @Override
+    public void createUser(UserDetails userData) {
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+    @Override
+    public void updateUser(UserDetails userData) {
+        throw new UnsupportedOperationException("Unimplemented method 'updateUser");
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        throw new UnsupportedOperationException("Unimplemented method 'changePassword'");
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        throw new UnsupportedOperationException("Unimplemented method 'userExists'");
+    }
+
+
+}
+// END
